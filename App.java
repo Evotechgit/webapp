a2e03d77cc04035571a15e80daa113759e5e8e7a
Evotechgit <evotechgit@gmail.com>
2016-06-28 14:16:15 +0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,14 @@
+package com.evotech.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Util.printMessage("Hello World!");
+    }
+}
+
---
+++ Util.java
@@ -0,0 +1,8 @@
+package com.evotech.web;
+
+public class Util 
+{
+   public static void printMessage(String message){
+	   System.out.println(message);
+   }
+}
